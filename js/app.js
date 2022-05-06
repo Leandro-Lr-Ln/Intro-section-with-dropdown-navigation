06a109dda6f5a7367b55a82c7dee16bcf4c6a6a7
Leandrox77 <leandro.ln.dev@gmail.com>
2022-05-06 19:04:58 -0300

    First commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const features = document.getElementById('features');
+const company = document.getElementById('company');
+
+features.addEventListener('click', () => {
+    features.classList.toggle('open');
+});
+company.addEventListener('click', () => {
+    company.classList.toggle('open');
+});
+
+
+
+
+
+
+
